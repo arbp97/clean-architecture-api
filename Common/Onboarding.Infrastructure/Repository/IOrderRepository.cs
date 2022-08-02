eca19fd78152834acc8e7351a87249d4443944cc
ablangille <ablanguille@andreani.com>
2022-08-02 10:13:13 -0300

    repository initial implementation

--- Common/Onboarding.Infrastructure/Repository/IOrderRepository.cs
+++ Common/Onboarding.Infrastructure/Repository/IOrderRepository.cs
@@ -1,4 +1,12 @@
+using Onboarding.Domain.Entities;
+
 namespace Onboarding.Infrastructure.Repository
 {
-    public interface IOrderRepository { }
+    public interface IOrderRepository
+    {
+        Task<Order> GetOrderById(Guid id);
+        Task<IEnumerable<Order>> GetAllOrders();
+        Task<Order> AddOrder(Order order);
+        Task<Order> UpdateOrder(Order order);
+    }
 }
--- Common/Onboarding.Infrastructure/Repository/Implementation/OrderRepository.cs
+++ Common/Onboarding.Infrastructure/Repository/Implementation/OrderRepository.cs
@@ -1,4 +1,40 @@
+using Onboarding.Infrastructure.Persistence;
+using Onboarding.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
 namespace Onboarding.Infrastructure.Repository
 {
-    public class OrderRepository : IOrderRepository { }
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly OnboardingDbContext _context;
+
+        public OrderRepository(OnboardingDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Order> GetOrderById(Guid id)
+        {
+            return await _context.Orders.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Order>> GetAllOrders()
+        {
+            return await _context.Orders.ToListAsync();
+        }
+
+        public async Task<Order> AddOrder(Order order)
+        {
+            _context.Orders.Add(order);
+            _context.SaveChanges();
+            return await Task.FromResult(order);
+        }
+
+        public async Task<Order> UpdateOrder(Order order)
+        {
+            _context.Entry(order).State = EntityState.Modified;
+            _context.SaveChanges();
+            return await Task.FromResult(order);
+        }
+    }
 }
